e76600a9a9b18fde0d0233d221d962672a9255a1
Pawel Kuczek <pawel.kuczek@vp.pl>
2023-07-20 12:44:27 +0200

    Zadanie domowe tydzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Paweł";
+bool isMan = true;
+int age = 27;
+
+if (isMan == false && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33 && isMan == false)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isMan == true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine($"Cześć, oto Twoje dane: imię - {name}, wiek - {age}, płeć - {(isMan ? "mężczyzna" : "kobieta")}");
+}
+
