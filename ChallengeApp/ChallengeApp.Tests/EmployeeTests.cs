79d2fc27e353d450d30e1011bdc4e626042363a3
Pawel Kuczek <pawel.kuczek@vp.pl>
2023-07-22 01:37:42 +0200

    Zadanie domowe tydzień 7 poprawka

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -3,7 +3,38 @@ namespace ChallengeApp.Tests
     public class Tests
     {
         [Test]
-        public void WhenUserGetsAndLosesGrades_ShouldReturnCorrectSum()
+        public void WhenEmployeeGetsGrades_ShouldReturnCorrectPositiveResult()
+        {
+            //arrange
+            var user = new Employee("Marek", "Kolano", 24);
+            user.AddGrade(10);
+            user.AddGrade(9);
+
+            //act
+            var result = user.Result;
+
+            //assert
+            Assert.AreEqual(19, result);
+
+        }
+
+        [Test]
+        public void WhenEmployeeLosesGrades_ShouldReturnCorrectNegativeResult()
+        {
+            //arrange
+            var user = new Employee("Marek", "Kolano", 24);
+            user.SubtractGrade(7);
+            user.SubtractGrade(6);
+
+            //act
+            var result = user.Result;
+
+            //assert
+            Assert.AreEqual(-13, result);
+
+        }
+        [Test]
+        public void WhenEmployeeGetsAndLosesGrades_ShouldReturnCorrectResult()
         {
             //arrange
             var user = new Employee("Marek", "Kolano", 24);
@@ -13,10 +44,8 @@ namespace ChallengeApp.Tests
             user.AddGrade(6);
             user.SubtractGrade(4);
 
-
             //act
             var result = user.Result;
-
 
             //assert
             Assert.AreEqual(12, result);
