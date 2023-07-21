688385ababff519939189463e27de2bbb3b16cc5
Pawel Kuczek <pawel.kuczek@vp.pl>
2023-07-21 11:43:23 +0200

    Zadanie domowe tydzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,69 +1,41 @@
-﻿int number = 99887766;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+﻿using ChallengeApp;
 
-foreach (char letter in letters)
+var employee1 = new Employee("Piotr", "Kozioł", 56);
+var employee2 = new Employee("Karol", "Krawczyk", 42);
+var employee3 = new Employee("Grażyna", "Żarko", 39);
+
+employee1.AddGrade(9);
+employee1.AddGrade(7);
+employee1.AddGrade(4);
+employee1.AddGrade(5);
+employee1.AddGrade(8);
+
+employee2.AddGrade(10);
+employee2.AddGrade(10);
+employee2.AddGrade(10);
+employee2.AddGrade(10);
+employee2.AddGrade(10);
+
+employee3.AddGrade(4);
+employee3.AddGrade(7);
+employee3.AddGrade(2);
+employee3.AddGrade(9);
+employee3.AddGrade(9);
+
+Employee[] employees = { employee1, employee2, employee3 };
+List<int> employeesResults = new List<int>();
+
+foreach (Employee employee in employees)
 {
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
-};
+    employeesResults.Add(employee.Result);
+}
 
-Console.WriteLine($"Ilość poszczególnych cyfr w liczbie: {number}");
-Console.WriteLine($"0 => {counter0}");
-Console.WriteLine($"1 => {counter1}");
-Console.WriteLine($"2 => {counter2}");
-Console.WriteLine($"3 => {counter3}");
-Console.WriteLine($"4 => {counter4}");
-Console.WriteLine($"5 => {counter5}");
-Console.WriteLine($"6 => {counter6}");
-Console.WriteLine($"7 => {counter7}");
-Console.WriteLine($"8 => {counter8}");
-Console.WriteLine($"9 => {counter9}");
+int bestEmployeeResult = employeesResults.Max();
+
+foreach (Employee employee in employees)
+{
+    if (bestEmployeeResult == employee.Result)
+    {
+        Console.WriteLine($"Najlepszy wynik wśród pracowników: {bestEmployeeResult}. Należy do: {employee.Name} {employee.Surname}, lat {employee.Age}");
+    }
+}
